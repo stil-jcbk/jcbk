90aad28d1044bff828d34adf51ede2b2e1a707ff
Jakub Konieczny <53277197+stil-jcbk@users.noreply.github.com>
2023-02-06 21:40:49 +0100

    changed the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'work-sans': ['"Work Sans"', 'cursive']
+        'work-sans': ['"Work Sans"', ...defaultTheme.fontFamily.serif]
       }
     },
   },
