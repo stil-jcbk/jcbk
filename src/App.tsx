7a9585856b28dc1a8b4d1ed9aed0c1c19166a188
Jakub Konieczny <53277197+stil-jcbk@users.noreply.github.com>
2023-02-06 20:57:21 +0100

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/navbar";
+import Title from "./components/Title/title";
+import ProjectsSection from "./sections/ProjectsSection";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
       </header>
+      <Title />
+      <ProjectsSection />
     </div>
   );
 }
