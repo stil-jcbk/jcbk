cb063bce1712d2a5ffdc02a68c8af7bee1d3caea
Jakub Konieczny <53277197+stil-jcbk@users.noreply.github.com>
2023-02-07 00:01:22 +0100

    added xmlns attr to svg

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -56,6 +56,7 @@ function Title() {
         id="scroll-arrow"
         className="w-12 animate-bounce transition-opacity"
         viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
       >
         <path d="m18.707 12.707-1.414-1.414L13 15.586V6h-2v9.586l-4.293-4.293-1.414 1.414L12 19.414z"></path>
       </svg>
