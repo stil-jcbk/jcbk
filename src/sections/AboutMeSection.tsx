b5c6da3113930701931a9cd08d39faaa9d352b3f
Jakub Konieczny <53277197+stil-jcbk@users.noreply.github.com>
2023-06-29 00:04:38 +0200

    about me section

---
+++ src/sections/AboutMeSection.tsx
@@ -0,0 +1,7 @@
+export default function AboutMeSection() {
+  return (
+    <div id="aboutme-section" className="mt-32">
+      <h2 className="mb-5 text-center text-3xl">ABOUT ME</h2>
+    </div>
+  );
+}
