d00c8b02632a5a8f9772415a24eaa97650949ef9
stil-jcbk <kubkon134@gmail.com>
2023-11-17 19:44:44 +0100

    about section content

--- src/sections/AboutMeSection.tsx
+++ src/sections/AboutMeSection.tsx
@@ -1,7 +1,60 @@
+const tools = {
+  front: ["react", "ts", "js", "html", "css", "sass"],
+  back: ["firebase", "nodejs", "express"],
+  other: ["git", "github"],
+};
+
 export default function AboutMeSection() {
   return (
     <div id="aboutme-section" className="mt-32">
       <h2 className="mb-5 text-center text-3xl">ABOUT ME</h2>
+      <div className="content flex justify-center gap-12">
+        <img
+          className="aspect-[9/16] h-auto w-1/4 object-contain"
+          src="/savecash.png"
+          alt=""
+        />
+        <div className="right">
+          <div className="mt-10">
+            <h3 className="text-5xl">Hi! I'm Jakub</h3>
+            <p className="text-justify text-lg">
+              My goal is to make user-friendly websites that make life easier.
+            </p>
+          </div>
+          <div className="tools mt-10 flex gap-10">
+            <div className="front text-center">
+              <span>front</span>
+              {tools.front.map((tool) => (
+                <img
+                  className="mb-2 w-11"
+                  src={`/assets/tools/${tool}.svg`}
+                  alt={`${tool} icon`}
+                />
+              ))}
+            </div>
+            <div className="back text-center">
+              <span>back</span>
+              {tools.back.map((tool) => (
+                <img
+                  className="mb-2 h-10 "
+                  src={`/assets/tools/${tool}.svg`}
+                  alt={`${tool} icon`}
+                />
+              ))}
+            </div>
+            <div className="other text-center">
+              <span>other</span>
+              {tools.other.map((tool) => (
+                <img
+                  className="mb-2 h-10 "
+                  src={`/assets/tools/${tool}.svg`}
+                  alt={`${tool} icon`}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
